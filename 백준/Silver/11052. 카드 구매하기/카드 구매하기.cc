b79ef6be21fe5e00b8cd92ef06b819765690fcd4
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2023-11-16 17:35:40 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int dp[1001];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> dp[i];
+
+        for (int j = 1; j <= i / 2; j++) {
+            dp[i] = max(dp[i], dp[j] + dp[i - j]);
+        }
+    }
+
+    cout << dp[n];
+    
+    return 0;
+}
