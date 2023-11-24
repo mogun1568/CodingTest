840d2e29731c80c70d93ee0345ac3f9da198fee3
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2023-11-24 09:09:26 +0900

    [Silver I] Title: 경로 찾기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+bool grape[100][100];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grape[i][j];
+        }
+    }
+
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grape[i][k] && grape[k][j]) {
+                    grape[i][j] = true;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << grape[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
