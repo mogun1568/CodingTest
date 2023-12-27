2895625c82714515ba7d1fcb4733400822c83cf2
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2023-12-27 22:09:26 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 216 ms, Memory: 2372 KB -BaekjoonHub

---
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int a[300][300];
+
+int dr[4] = {0, 1, 0, -1};
+int dc[4] = {1, 0, -1, 0};
+
+void rotate(int box) {
+    for (int i = 0; i < box; i++) {
+        int startValue = a[i][i];
+        int r = i;
+        int c = i;
+        int dir = 0;
+
+        while (1) {
+            int nr = r + dr[dir];
+            int nc = c + dc[dir];
+
+            if (nr == i && nc == i) {
+                break;
+            }
+
+            if (nr < i || nr >= n - i || nc < i || nc >= m - i) {
+                dir++;
+                continue;
+            } 
+
+            a[r][c] = a[nr][nc];
+            r = nr;
+            c = nc;
+        }
+
+        a[i + 1][i] = startValue;
+    }
+    
+
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int r;
+    cin >> n >> m >> r;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < r; i++) {
+        rotate(min(n, m) / 2);
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
