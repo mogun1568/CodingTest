013f082c5e9fe769b3d1f80ec6611036a59f82d3
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2023-12-30 18:52:48 +0900

    [Silver I] Title: 하노이 탑, Time: 132 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+void hanoi(int n, int start, int mid, int end){
+    if(n == 1){
+        cout << start << " " << end << "\n";
+        return;
+    }
+    
+    hanoi(n - 1, start, end, mid);
+    cout << start << " " << end << "\n";
+    hanoi(n - 1, mid, start, end);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+
+    string k = to_string(pow(2, n));
+    int idx = k.find('.');
+    k = k.substr(0, idx);
+    k[k.length() - 1] -= 1;	
+    cout << k << "\n";
+
+    if (n <= 20) {
+        hanoi(n, 1, 2, 3);
+    }
+    
+    return 0;
+}
