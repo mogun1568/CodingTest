8084849d62747d54d296f4f193666da3904d36e5
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2024-02-13 18:37:24 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t, x, y;
+    cin >> t;
+
+    while (t--) {
+        cin >> x >> y;
+
+        double s = sqrt(y - x);
+        int r = round(s);
+        
+        if (r >= s) {
+            cout << 2 * r - 1 << "\n";
+        } else {
+            cout << 2 * r << "\n";
+        }
+    }
+    
+    return 0;
+}
