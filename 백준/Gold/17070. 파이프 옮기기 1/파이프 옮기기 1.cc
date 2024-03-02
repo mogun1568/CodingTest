85103e4a5bc6a9ee9b3181d868adae6aed3345a6
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2024-03-02 22:36:40 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -2,47 +2,42 @@
 
 using namespace std;
 
-int n, cnt;
-int house[17][17];
-
-void dfs(int r, int c, int d) {
-    if (r == n && c == n) {
-        cnt++;
-    }
-
-    if (d != 2) {
-        if (c + 1 <= n && house[r][c + 1] != 1) {
-            dfs(r, c + 1, 1);
-        }
-    }
-
-    if (d != 1) {
-        if (r + 1 <= n && house[r + 1][c] != 1) {
-            dfs(r + 1, c, 2);
-        }
-    }
-
-    if (r + 1 <= n && c + 1 <= n) {
-        if (house[r][c + 1] != 1 && house[r + 1][c] != 1 && house[r + 1][c + 1] != 1) {
-            dfs(r + 1, c + 1, 3);
-        }    
-    }
-}
+bool house[17][17];
+int dp[17][17][3];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
+    int n;
     cin >> n;
+
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
             cin >> house[i][j];
         }
     }
 
-    dfs(1, 2, 1);
+    dp[1][2][0] = 1;
 
-    cout << cnt;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 3; j <= n; j++) {
+            if (house[i][j]) {
+                continue;
+            }
+
+            dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+            dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+
+            if (house[i - 1][j] || house[i][j - 1]) {
+                continue;
+            }
+
+            dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+        }
+    }
+
+    cout << dp[n][n][0] + dp[n][n][1] + dp[n][n][2];
 
     return 0;
 }
