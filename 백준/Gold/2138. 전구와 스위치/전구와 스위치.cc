c53c9d9d64fd62c28cddebcaff4b7d7fa3a8f4c0
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2024-04-27 19:58:43 +0900

    [Gold V] Title: 전구와 스위치, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.cc
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.cc
@@ -1,58 +1,47 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
+#define MAX 987654321
 
 using namespace std;
 
-#define MAX 987654321
+int n,ans=MAX,cnt=0;
+string st, dt, temp;
 
-int n, answer = MAX;
-string ss, es, s;
-
-void SwitchClick(int idx) {
-    s[idx - 1] = (s[idx - 1] == '0') ? '1' : '0';
-    s[idx] = (s[idx] == '0') ? '1' : '0';
-    if (idx + 1 < n) {
-        s[idx + 1] = (s[idx + 1] == '0') ? '1' : '0';
-    }
+void input(){
+    cin >> n;
+    cin >> st;
+    cin >> dt;
 }
 
-void Solve(int def) {
-    s = ss;
-    int cnt = def;
-    
-    for (int i = 1; i < n; i++) {
-        if (s[i - 1] != es[i - 1]) {
-            SwitchClick(i);
+void lightOn(int idx){
+    if(idx>0) temp[idx-1] = (temp[idx-1]=='0') ? '1' : '0';
+    temp[idx] = (temp[idx]=='0') ? '1' : '0';
+    if(idx<n-1) temp[idx+1] = (temp[idx+1]=='0') ? '1' : '0';
+}
+
+void solve(int first){
+    temp = st;
+    cnt = 0;
+    if(first==0){
+        temp[0] = (temp[0]=='0') ? '1' : '0';
+        temp[1] = (temp[1]=='0') ? '1' : '0';
+        cnt++;
+    }
+    for(int i=1; i<n; i++){
+        if(temp[i-1]!=dt[i-1]){
+            lightOn(i);
             cnt++;
         }
-
-        if (s == es) {
-            answer = min(answer, cnt);
-            return;
-        }
     }
-
-    return;
+    if(temp==dt) ans = min(ans,cnt);
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    
-    cin >> n >> ss >> es;
-
-    Solve(0);
-
-    ss[0] = (ss[0] == '0') ? '1' : '0';
-    ss[1] = (ss[1] == '0') ? '1' : '0';
-    Solve(1);
-    
-    if (answer != MAX) {
-        cout << answer;
-    } else {
-        cout << -1;
-    }
-    
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    input();
+    solve(0);
+    solve(1);
+    if(ans!=MAX) cout << ans << "\n";
+    else cout << -1 << "\n";
     return 0;
 }
