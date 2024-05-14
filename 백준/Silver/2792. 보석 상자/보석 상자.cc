69558f8e17aae8f5f14cd6674227f4cea70aba83
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2024-05-14 19:29:37 +0900

    [Silver I] Title: 보석 상자, Time: 80 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Silver/2792. 보석 상자/보석 상자.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int N, M;
+int box[300000];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> N >> M;
+    for (int i = 0; i < M; i++) {
+        cin >> box[i];
+    }
+
+    int start = 1, end = *max_element(box, box + M), mid;
+
+    while (start <= end) {
+        mid = (start + end) / 2;
+
+        int cnt = 0;
+
+        for (int i = 0; i < M; i++) {
+            cnt += ceil((double)box[i] / mid);
+        }
+
+        if (cnt > N) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+
+    cout << start;
+
+    return 0;
+}
