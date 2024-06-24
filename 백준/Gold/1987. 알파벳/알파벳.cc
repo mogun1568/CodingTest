9845abe6d6f97ebf9d7af4bfa8610a6e294e23aa
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2024-06-24 18:54:46 +0900

    [Gold IV] Title: 알파벳, Time: 496 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int R, C, answer;
+char board[20][20];
+bool visited[20][20];
+bool alpha[26];
+
+int dr[4] = {-1, 1, 0, 0};
+int dc[4] = {0, 0, -1, 1};
+
+void Dfs(int r, int c, int cnt) {
+    answer = max(answer, cnt);
+    
+    for (int i = 0; i < 4; i++) {
+        int nr = r + dr[i];
+        int nc = c + dc[i];
+
+        if (nr < 0 || nr >= R || nc < 0 || nc >= C)
+            continue;
+
+        int idx = board[nr][nc] - 'A';
+        if (visited[nr][nc] || alpha[idx])
+            continue;
+
+        visited[nr][nc] = true;
+        alpha[idx] = true;
+        Dfs(nr, nc, cnt + 1);
+        visited[nr][nc] = false;
+        alpha[idx] = false;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> R >> C;
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    visited[0][0] = true;
+    alpha[board[0][0] - 'A'] = true;
+    Dfs(0, 0, 1);
+
+    cout << answer;
+    
+    return 0;
+}
