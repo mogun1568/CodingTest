22928f884d7140585ff69303b81e578ece32faa3
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2024-07-09 19:41:13 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2340 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -6,41 +6,33 @@
 using namespace std;
 
 int N, M, X, answer;
-vector<pair<int, int>> v[1001];
-int visited[1001];
+vector<pair<int, int>> v[2][1001];
+int visited[2][1001];
 
-void Init() {
-    for (int i = 1; i <= N; i++)
-        visited[i] = 1000000;
-}
-
-int Dijkstra(int start, int end) {
-    Init();
+void Dijkstra(int dir) {
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    visited[start] = 0;
-    pq.push({0, start});
+    visited[dir][X] = 0;
+    pq.push({0, X});
 
     while (!pq.empty()) {
         int ct = pq.top().first;
         int cv = pq.top().second;
         pq.pop();
 
-        if (cv == end)
-            return ct;
+        if (ct > visited[dir][cv])
+            continue;
 
-        for (int i = 0; i < v[cv].size(); i++) {
-            int nt = ct + v[cv][i].second;
-            int nv = v[cv][i].first;
+        for (int i = 0; i < v[dir][cv].size(); i++) {
+            int nt = ct + v[dir][cv][i].second;
+            int nv = v[dir][cv][i].first;
 
-            if (nt >= visited[nv])
+            if (nt >= visited[dir][nv])
                 continue;
 
-            visited[nv] = nt;
+            visited[dir][nv] = nt;
             pq.push({nt, nv});
         }
     }
-
-    return 0;
 }
 
 int main() {
@@ -52,14 +44,21 @@ int main() {
     int a, b, t;
     for (int i = 0; i < M; i++) {
         cin >> a >> b >> t;
-        v[a].push_back({b, t});
+        v[0][a].push_back({b, t});
+        v[1][b].push_back({a, t});
     }
+
+    for (int i = 1; i <= N; i++) {
+        visited[0][i] = 1000000;
+        visited[1][i] = 1000000;
+    }
+
+    Dijkstra(0);
+    Dijkstra(1);
 
     int time;
     for (int i = 1; i <= N; i++) {
-        time = Dijkstra(i, X);
-        time += Dijkstra(X, i);
-
+        time = visited[0][i] + visited[1][i];
         answer = max(answer, time);
     }
 
