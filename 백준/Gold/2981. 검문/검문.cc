efc3dc28cd316b0ff4aa5ceb6547d303da86f60f
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2024-10-16 19:48:31 +0900

    [Gold IV] Title: 검문, Time: 576 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2981. 검문/검문.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, gcd;
+int arr[100];
+int diff[99];
+
+int Gcd(int a, int b) {
+	int c = a % b;
+	while (c != 0) {
+		a = b;
+		b = c;
+		c = a % b;
+	}
+	return b;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+    sort(arr, arr + N);
+
+    for (int i = 0; i < N - 1; i++)
+        diff[i] = arr[i + 1] - arr[i];
+
+    gcd = diff[0];
+    for (int i = 1; i < N - 1; i++)
+        gcd = Gcd(max(gcd, diff[i]), min(gcd, diff[i]));
+
+    for (int i = 2; i <= gcd / 2; i++) {
+        if (gcd % i == 0)
+            cout << i << "\n";
+    }
+    cout << gcd;
+
+    return 0;
+}
