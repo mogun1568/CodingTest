00528c13e140b1ab6395272251048b0ddaf70fde
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2024-10-23 16:49:31 +0900

    [Gold IV] Title: 키 순서, Time: 88 ms, Memory: 3000 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.cc
+++ 백준/Gold/2458. 키 순서/키 순서.cc
@@ -6,8 +6,7 @@ using namespace std;
 #define INF 500
 
 int N, M, ans;
-int fw1[501][501];
-int fw2[501][501];
+int fw[501][501];
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -17,27 +16,22 @@ int main() {
     
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
-            fw1[i][j] = INF;
-            if (i == j) fw1[i][j] = 0;
-            fw2[i][j] = INF;
-            if (i == j) fw2[i][j] = 0;
+            fw[i][j] = INF;
+            if (i == j) fw[i][j] = 0;
         }
     }
     
     int a, b;
     for (int i = 0; i < M; i++) {
         cin >> a >> b;
-        fw1[a][b] = 1;
-        fw2[b][a] = 1;
+        fw[a][b] = 1;
     }
 
     for (int k = 1; k <= N; k++) {
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                if (fw1[i][k] + fw1[k][j] < fw1[i][j])
-                    fw1[i][j] = fw1[i][k] + fw1[k][j];
-                if (fw2[i][k] + fw2[k][j] < fw2[i][j])
-                    fw2[i][j] = fw2[i][k] + fw2[k][j];
+                if (fw[i][k] + fw[k][j] < fw[i][j])
+                    fw[i][j] = fw[i][k] + fw[k][j];
             }
         }
     }
@@ -45,7 +39,7 @@ int main() {
     for (int i = 1; i <= N; i++) {
         int cnt = 0;
         for (int j = 1; j <= N; j++) {
-            if (fw1[i][j] != INF || fw2[i][j] != INF)
+            if (fw[i][j] != INF || fw[j][i] != INF)
                 cnt++;
         }
 
