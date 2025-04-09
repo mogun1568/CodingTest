9ec9a1a147ba426ccc67e6e590cbdf21b7a1af99
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-04-09 20:13:22 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -3,25 +3,23 @@
 
 using namespace std;
 
-int dp[1000][3];
+int RGB[1001][3];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    int n;
-    cin >> n;
 
-    cin >> dp[0][0] >> dp[0][1] >> dp[0][2];
-    for (int i = 1; i < n; i++) {
-        cin >> dp[i][0] >> dp[i][1] >> dp[i][2];
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> RGB[i][0] >> RGB[i][1] >> RGB[i][2];
 
-        dp[i][0] = min(dp[i - 1][1] + dp[i][0], dp[i - 1][2] + dp[i][0]);
-        dp[i][1] = min(dp[i - 1][0] + dp[i][1], dp[i - 1][2] + dp[i][1]);
-        dp[i][2] = min(dp[i - 1][0] + dp[i][2], dp[i - 1][1] + dp[i][2]);
+        RGB[i][0] += min(RGB[i - 1][1], RGB[i - 1][2]);
+        RGB[i][1] += min(RGB[i - 1][0], RGB[i - 1][2]);
+        RGB[i][2] += min(RGB[i - 1][0], RGB[i - 1][1]);
     }
 
-    cout << min(dp[n - 1][0], min(dp[n - 1][1], dp[n - 1][2]));
+    cout << min(RGB[N][0], min(RGB[N][1], RGB[N][2]));
     
     return 0;
 }
