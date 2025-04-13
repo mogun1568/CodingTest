dfd0c1afb9f9f1a6481a47cb19d89532e9c96949
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-04-13 16:30:56 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,38 +1,35 @@
 #include <iostream>
-
 using namespace std;
-
 #define DIV 1000000000
-int dp[101][10];
+
+int DP[101][10];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, sum = 0;
-    cin >> n;
+    int N;
+    cin >> N;
 
-    for (int i = 1; i < 10; i++) {
-        dp[1][i] = 1;
-    }
+    for (int i = 1; i < 10; i++)
+        DP[1][i] = 1;
 
-    for (int i = 2; i <= n; i++) {
+    for (int i = 2; i <= N; i++) {
         for (int j = 0; j < 10; j++) {
-            if (j == 0) {
-                dp[i][j] = dp[i - 1][1] % DIV;
-            } else if (j == 9) {
-                dp[i][j] = dp[i - 1][8] % DIV;
-            } else {
-                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % DIV;
-            }
+            if (j == 0)
+                DP[i][j] = DP[i - 1][1] % DIV;
+            else if (j == 9)
+                DP[i][j] = DP[i - 1][8] % DIV;
+            else
+                DP[i][j] = (DP[i - 1][j - 1] % DIV + DP[i - 1][j + 1] % DIV) % DIV;
         }
     }
 
-    for (int i = 0; i < 10; i++) {
-        sum = (sum + dp[n][i]) % DIV;
-    }
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+        sum = (sum + DP[N][i]) % DIV;
 
     cout << sum;
-
+    
     return 0;
 }
