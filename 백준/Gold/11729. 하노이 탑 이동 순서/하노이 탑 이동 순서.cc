07a99a7a37ba83d25a06dffdad5a23551d9306b7
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-04-18 03:02:48 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 116 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -1,30 +1,26 @@
 #include <iostream>
-#include <string>
-#include <cmath>
-
 using namespace std;
 
-void hanoi(int n, int start, int mid, int end){
-    if(n == 1){
+void Hanoi(int n, int start, int mid, int end) {
+    if (n == 1) {
         cout << start << " " << end << "\n";
         return;
     }
-    
-    hanoi(n - 1, start, end, mid);
+
+    Hanoi(n - 1, start, end, mid);
     cout << start << " " << end << "\n";
-    hanoi(n - 1, mid, start, end);
+    Hanoi(n - 1, mid, start, end);
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    int n;
-    cin >> n;
 
-    cout << (1 << n) - 1 << "\n";
+    int N;
+    cin >> N;
 
-    hanoi(n, 1, 2, 3);
+    cout << (1 << N) - 1 << "\n";
+    Hanoi(N, 1, 2, 3);
     
     return 0;
 }
