cf79e97b55d835592e216a11bedb8571531cbb17
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-04-18 21:05:54 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -1,39 +1,29 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
+#define INF 1000000000;
 
-int a[11];
-int op[4];
-int n, maxResult = -1000000001, minResult = 1000000001;
+int N, ansMax = -1000000000, ansMin = 1000000000;
+int arr[11];
+int operation[4];
 
-void dfs(int result, int aIdx) {
-    if (aIdx == n) {
-        if (result > maxResult) {
-            maxResult = result;
-        }
-        if (result < minResult) {
-            minResult = result;
-        }
-
+void DFS(int idx, int ans) {
+    if (idx == N - 1) {
+        ansMax = max(ansMax, ans);
+        ansMin = min(ansMin, ans);
         return;
     }
 
     for (int i = 0; i < 4; i++) {
-        if (op[i] > 0) {
-            op[i]--;
+        if (operation[i] == 0)
+            continue;
 
-            if (i == 0) {
-                dfs(result + a[aIdx], aIdx + 1);
-            } else if (i == 1) {
-                dfs(result - a[aIdx], aIdx + 1);
-            } else if (i == 2) {
-                dfs(result * a[aIdx], aIdx + 1);
-            } else {
-                dfs(result / a[aIdx], aIdx + 1);
-            }
-
-            op[i]++;
-        }
+        operation[i]--;
+        if (i == 0) DFS(idx + 1, ans + arr[idx + 1]);
+        else if (i == 1) DFS(idx + 1, ans - arr[idx + 1]);
+        else if (i == 2) DFS(idx + 1, ans * arr[idx + 1]);
+        else DFS(idx + 1, ans / arr[idx + 1]);
+        operation[i]++;
     }
 }
 
@@ -41,19 +31,15 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    cin >> n;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+    for (int i = 0; i < 4; i++)
+        cin >> operation[i];
 
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
+    DFS(0, arr[0]);
 
-    for (int i = 0; i < 4; i++) {
-        cin >> op[i];
-    }
-
-    dfs(a[0], 1);
-
-    cout << maxResult << "\n" << minResult;
-
+    cout << ansMax << "\n" << ansMin;
+    
     return 0;
 }
