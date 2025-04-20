f5502120187dcd0364708cbcb3b9f3a61b85c503
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-04-20 17:59:29 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.cc
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -1,74 +1,51 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
-vector<int> tree[91]; // 아스키 코드 90 -> Z
+char tree[26][2];
 
-void pre(char ap) {
-    cout << ap;
-
-    if (tree[ap][0] != '.') {
-        pre(tree[ap][0]);
-    }
+void Pre(char cur) {
+    if (cur == '.')
+        return;
     
-    if (tree[ap][1] != '.') {
-        pre(tree[ap][1]);
-    }
+    cout << cur;
+    Pre(tree[cur - 'A'][0]);
+    Pre(tree[cur - 'A'][1]);
 }
 
-void in(char ap) {
-    if (tree[ap][0] != '.') {
-        in(tree[ap][0]);
-    }
-
-    cout << ap;
+void In(char cur) {
+    if (cur == '.')
+        return;
     
-    if (tree[ap][1] != '.') {
-        in(tree[ap][1]);
-    }
+    In(tree[cur - 'A'][0]);
+    cout << cur;
+    In(tree[cur - 'A'][1]);
 }
 
-void post(char ap) {
-    if (tree[ap][0] != '.') {
-        post(tree[ap][0]);
-    }
-
-    if (tree[ap][1] != '.') {
-        post(tree[ap][1]);
-    }
-
-    cout << ap;
+void Post(char cur) {
+    if (cur == '.')
+        return;
+    
+    Post(tree[cur - 'A'][0]);
+    Post(tree[cur - 'A'][1]);
+    cout << cur;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
-    int n;
-    cin >> n;
-
-    char p, l, r;
-    for (int i = 0; i < n; i++) {
-        cin >> p >> l >> r;
-
-        if (l != '.') {
-            tree[p].push_back(l);
-        } else {
-            tree[p].push_back('.');
-        }
-        if (r != '.') {
-            tree[p].push_back(r);
-        } else {
-            tree[p].push_back('.');
-        }
-    }
     
-    pre('A');
+    int N;
+    cin >> N;
+
+    char root;
+    for (int i = 0; i < N; i++)
+        cin >> root >> tree[root - 'A'][0] >> tree[root - 'A'][1];
+        
+    Pre('A');
     cout << "\n";
-    in('A');
+    In('A');
     cout << "\n";
-    post('A');
+    Post('A');
     
     return 0;
 }
