4dfdd46d18238dbce051a6f0b2274e74d5721f02
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-04-25 21:21:17 +0900

    [Gold V] Title: 토마토, Time: 88 ms, Memory: 7524 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -1,77 +1,72 @@
 #include <iostream>
 #include <queue>
-
 using namespace std;
 
-int box[100][100][100];
-
-int ds[6] = {-1, 1, 0, 0, 0, 0};
-int dr[6] = {0, 0, 0, 0, 1, -1};
-int dc[6] = {0, 0, -1, 1, 0, 0};
-
 struct tomato {
-    int s;
+    int f;
     int r;
     int c;
 };
+
+int M, N, H, ans, cnt;
+int box[100][100][100];
+queue<tomato> q;
+
+int df[6] = {-1, 1, 0, 0, 0, 0};
+int dr[6] = {0, 0, 0, 0, 1, -1};
+int dc[6] = {0, 0, -1, 1, 0, 0};
+
+void BFS() {
+    while (!q.empty()) {
+        int cf = q.front().f;
+        int cr = q.front().r;
+        int cc = q.front().c;
+        q.pop();
+
+        ans = box[cf][cr][cc] - 1;
+        cnt--;
+
+        for (int i = 0; i < 6; i++) {
+            int nf = cf + df[i];
+            int nr = cr + dr[i];
+            int nc = cc + dc[i];
+
+            if (nf < 0 || nf >= H || nr < 0 || nr >= N || nc < 0 || nc >= M)
+                continue;
+
+            if (box[nf][nr][nc] != 0)
+                continue;
+
+            box[nf][nr][nc] = box[cf][cr][cc] + 1;
+            q.push({nf, nr, nc});
+        }      
+    }
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
-    int m, n, h, cnt, result;
-    cin >> m >> n >> h;
-
-    cnt = m * n * h;
-    queue<tomato> q;
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < m; k++) {
+    cin >> M >> N >> H;
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < M; k++) {
                 cin >> box[i][j][k];
 
-                if (box[i][j][k] == 1) {
+                if (box[i][j][k] == 1)
                     q.push({i, j, k});
-                }
-
-                if (box[i][j][k] != 0) {
-                    cnt--;
-                }
+                if (box[i][j][k] != -1)
+                    cnt++;
             }
         }
     }
 
-    while (!q.empty()) {
-        int s = q.front().s;
-        int r = q.front().r;
-        int c = q.front().c;
-        q.pop();
+    BFS();
 
-        result = box[s][r][c] - 1;
-
-        for (int i = 0; i < 6; i++) {
-            int ns = s + ds[i];
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-
-            if (ns < 0 || ns >= h || nr < 0 || nr >= n || nc < 0 || nc >= m) {
-                continue;
-            }
-
-            if (box[ns][nr][nc] != 0) {
-                continue;
-            }
-
-            box[ns][nr][nc] = box[s][r][c] + 1;
-            q.push({ns, nr, nc});
-            cnt--;
-        }
-    }
-
-    if (cnt > 0) {
+    if (cnt == 0)
+        cout << ans;
+    else
         cout << -1;
-    } else {
-        cout << result;
-    }
     
     return 0;
 }
