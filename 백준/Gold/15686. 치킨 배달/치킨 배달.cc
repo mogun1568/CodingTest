ace6254c63867fcd3bbd7cd8a500b492d166d915
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-05-01 02:07:03 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,65 +1,60 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <algorithm>
-
 using namespace std;
 
-int m, chiLen, result = 20000;
+int M, chickenCnt, ans = 10000;
 vector<pair<int, int>> house, chicken;
-bool checkChi[13];
+bool ischicken[13];
 
-void dfs(int cnt, int idx) {
-    if (chiLen - cnt == m) {
-        int sum = 0;
+void DFS(int idx, int cnt) {
+    if (cnt == M) {
+        int sum = 0, dis;
         for (auto h : house) {
-            int dis = 110;
-            for (int i = 0; i < chiLen; i++) {
-                if (checkChi[i]) {
+            dis = 100;
+            for (int i = 0; i < chickenCnt; i++) {
+                if (!ischicken[i])
                     continue;
-                }
-                                
+
                 dis = min(dis, abs(h.first - chicken[i].first) + abs(h.second - chicken[i].second));
             }
-    
+
             sum += dis;
         }
 
-        result = min(result, sum);
+        ans = min(ans, sum);
         
         return;
     }
 
-    for (int i = idx; i < chiLen; i++) {
-        checkChi[i] = true;
-        dfs(cnt + 1, i + 1);
-        checkChi[i] = false;
+    for (int i = idx; i < chickenCnt; i++) {
+        ischicken[i] = true;
+        DFS(i + 1, cnt + 1);
+        ischicken[i] = false;
     }
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    int n, a;
-    cin >> n >> m;
-    
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+
+    int N, a;
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             cin >> a;
 
-            if (a == 1) {
+            if (a == 1)
                 house.push_back({i, j});
-            } else if (a == 2) {
+            else if (a == 2)
                 chicken.push_back({i, j});
-            }
         }
     }
-    chiLen = chicken.size();
+    chickenCnt = chicken.size();
 
-    dfs(0, 0);
+    DFS(0, 0);
 
-    cout << result;
+    cout << ans;
     
     return 0;
 }
