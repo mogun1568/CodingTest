8dc60bac45a1b706db64d60e0910027f1dc27b31
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-05-03 22:22:24 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 108 ms, Memory: 6124 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,49 +1,28 @@
 #include <iostream>
-
 using namespace std;
 
-int dp[1025][1025];
+int DP[1025][1025];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
-    int n, m;
-    cin >> n >> m;
+    int N, M;
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> DP[i][j];
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            cin >> dp[i][j];
-
-            if (i == 0 && j == 0) {
-                continue;
-            } else if (i == 0) {
-                dp[i][j] += dp[i][j - 1];
-            } else if (j == 0) {
-                dp[i][j] += dp[i - 1][j];
-            } else {
-                dp[i][j] += dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1];
-            }
+            DP[i][j] += DP[i - 1][j] + DP[i][j - 1] - DP[i - 1][j - 1];
         }
     }
 
-    int x1, y1, x2, y2, result;
-    for (int i = 0; i < m; i++) {
+    int x1, y1, x2, y2, ans;
+    for (int i = 0; i < M; i++) {
         cin >> x1 >> y1 >> x2 >> y2;
 
-        result = dp[x2][y2];
-
-        if (x1 - 1 > 0) {
-            result -= dp[x1 - 1][y2];
-        }
-        if (y1 - 1 > 0) {
-            result -= dp[x2][y1 - 1];
-        }
-        if (x1 - 1 > 0 && y1 - 1 > 0) {
-            result += dp[x1 - 1][y1 - 1];
-        }
-
-        cout << result << "\n";
+        ans = DP[x2][y2] - DP[x1 - 1][y2] - DP[x2][y1 - 1] + DP[x1 - 1][y1 - 1];
+        cout << ans << "\n";
     }
     
     return 0;
