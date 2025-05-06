38953c615ae544b4e29ec03c35863b3eac75dd6c
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-05-06 18:34:10 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.cc
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -1,49 +1,42 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-
 using namespace std;
 
-int l, c;
+int L, C;
 string s;
 
-void dfs(string pw, int idx, int consonant, int vowel) {
-    if (idx == c) {
-        if (pw.length() != l) {
-            return;
-        }
-
-        if (consonant < 2 || vowel < 1) {
-            return;
-        }
-
-        cout << pw << "\n";
-
-        return;
+void DFS(string pw, int idx, int consonant, int vowel) {
+    if (pw.length() == L) {
+        if (consonant > 1 && vowel > 0)
+            cout << pw << "\n";
+        
+        return;    
     }
 
-    if (s[idx] == 'a' || s[idx] == 'e' || s[idx] == 'i' || s[idx] == 'o' || s[idx] == 'u') {
-        dfs(pw + s[idx], idx + 1, consonant, vowel + 1);
-    } else {
-       dfs(pw + s[idx], idx + 1, consonant + 1, vowel);
+    for (int i = idx; i < C; i++) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            DFS(pw + s[i], i + 1, consonant, vowel + 1);
+        else
+            DFS(pw + s[i], i + 1, consonant + 1, vowel);
     }
-    dfs(pw, idx + 1, consonant, vowel);
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    cin >> l >> c;
 
-    char a;
-    for (int i = 0; i < c; i++) {
-        cin >> a;
-        s += a;
+    cin >> L >> C;
+
+    char c;
+    for (int i = 0; i < C; i++) {
+        cin >> c;
+        s += c;
     }
+
     sort(s.begin(), s.end());
 
-    dfs("", 0, 0, 0);
+    DFS("", 0, 0, 0);
     
     return 0;
 }
