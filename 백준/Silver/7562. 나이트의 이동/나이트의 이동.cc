e43de17912f51e267ab26600cd74f2e65860c27d
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-05-08 20:45:47 +0900

    [Silver I] Title: 나이트의 이동, Time: 28 ms, Memory: 2504 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -1,42 +1,42 @@
 #include <iostream>
-#include <queue>
 #include <cstring>
-
+#include <queue>
 using namespace std;
 
-int l, er, ec;
-int dr[8] = {1, 2, 2, 1, -1, -2, -2, -1};
-int dc[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
-int board[300][300];
-bool visit[300][300];
+int l;
+pair<int, int> knight[2];
+int visited[300][300];
 
-void bfs(int r, int c) {
+int dr[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
+int dc[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+
+void BFS() {
     queue<pair<int, int>> q;
-    q.push({r, c});
-    visit[r][c] = true;
+    visited[knight[0].first][knight[0].second] = 1;
+    q.push({knight[0].first, knight[0].second});
 
     while (!q.empty()) {
         int r = q.front().first;
         int c = q.front().second;
         q.pop();
 
+        if (r == knight[1].first && c == knight[1].second) {
+            cout << visited[r][c] - 1 << "\n";
+            return;
+        }
+
         for (int i = 0; i < 8; i++) {
             int nr = r + dr[i];
             int nc = c + dc[i];
 
-            if (nr < 0 || nr >= l || nc < 0 || nc >= l) {
+            if (nr < 0 || nr >= l || nc < 0 || nc >= l)
                 continue;
-            }
 
-            if (!visit[nr][nc]) {
-                q.push({nr, nc});
-                board[nr][nc] = board[r][c] + 1;
-                visit[nr][nc] = true;
-            }
+            if (visited[nr][nc] != 0)
+                continue;
 
-            if (nr == er && nc == ec) {
-                return;
-            }
+            visited[nr][nc] = visited[r][c] + 1;
+            q.push({nr, nc});
         }
     }
 }
@@ -45,17 +45,20 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int t, r, c;
-    cin >> t;
+    int T, r, c;
+    cin >> T;
 
-    while (t--) {
-        cin >> l >> r >> c >> er >> ec;
+    while (T--) {
+        cin >> l;
+        for (int i = 0; i < 2; i++) {
+            cin >> r >> c;
+            knight[i].first = r;
+            knight[i].second = c;
+        }
+        memset(visited, 0, sizeof(visited));
 
-        bfs(r, c);
-
-        cout << board[er][ec] << "\n";
-
-        memset(board, 0, sizeof(board));
-        memset(visit, false, sizeof(visit));
+        BFS();
     }
+    
+    return 0;
 }
