ce76650bd1215b2f855ac32d4d8c1feb61cb3cc1
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-05-13 19:57:20 +0900

    [Gold IV] Title: 테트로미노, Time: 84 ms, Memory: 3240 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.cc
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -1,48 +1,21 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-
 using namespace std;
 
-int N, M, answer;
+int N, M, ans;
 int paper[500][500];
 bool visited[500][500];
 
 int dr[4] = {-1, 1, 0, 0};
 int dc[4] = {0, 0, -1, 1};
 
-void Solve(int r, int c) {
-    vector<int> v;
-    for (int i = 0; i < 4; i++) {
-        int nr = r + dr[i];
-        int nc = c + dc[i];
-
-        if (nr < 0 || nr >= N || nc < 0 || nc >= M)
-            continue;
-
-        v.push_back(paper[nr][nc]);
-    }
-
-    if (v.size() < 3)
-        return;
-
-    int sum = paper[r][c];
-    for (int i = 0; i < v.size(); i++)
-        sum += v[i];
-
-    if (v.size() == 3) {
-        answer = max(answer, sum);
-        return;
-    }
-
-    answer = max(answer, sum - *min_element(v.begin(), v.end()));
-}
-
-void Dfs(int r, int c, int cnt, int sum) {
+void DFS(int r, int c, int cnt, int sum) {
     if (cnt == 4) {
-        answer = max(answer, sum);
+        ans = max(ans, sum);
         return;
     }
+
+    int tCnt = 0, tSum = 0, tMin = 1000;
 
     for (int i = 0; i < 4; i++) {
         int nr = r + dr[i];
@@ -51,37 +24,49 @@ void Dfs(int r, int c, int cnt, int sum) {
         if (nr < 0 || nr >= N || nc < 0 || nc >= M)
             continue;
 
-        if (visited[nr][nc] != 0)
+        if (visited[nr][nc])
             continue;
+
+        if (cnt == 2) {
+            tCnt++;
+            tSum += paper[nr][nc];
+            tMin = min(tMin, paper[nr][nc]);
+        }
 
         visited[nr][nc] = true;
-        Dfs(nr, nc, cnt + 1, sum + paper[nr][nc]);
+        DFS(nr, nc, cnt + 1, sum + paper[nr][nc]);
         visited[nr][nc] = false;
+    }
+
+    if (cnt == 2) {
+        if (tCnt < 2)
+            return;
+        else if (tCnt == 2)
+            ans = max(ans, sum + tSum);
+        else
+            ans = max(ans, sum + tSum - tMin);
     }
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
+
     cin >> N >> M;
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
+        for (int j = 0; j < M; j++)
             cin >> paper[i][j];
-        }
     }
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             visited[i][j] = true;
-            Dfs(i, j, 1, paper[i][j]);
+            DFS(i, j, 1, paper[i][j]);
             visited[i][j] = false;
-
-            Solve(i, j);
         }
     }
 
-    cout << answer;
+    cout << ans;
     
     return 0;
 }
