c072319ca928baaf3136b7d66ec030d2f63ef735
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-05-16 20:14:46 +0900

    [Silver I] Title: 경로 찾기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.cc
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 bool grape[100][100];
@@ -8,29 +7,25 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int n;
-    cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
             cin >> grape[i][j];
-        }
     }
 
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grape[i][k] && grape[k][j]) {
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grape[i][k] && grape[k][j])
                     grape[i][j] = true;
-                }
             }
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
             cout << grape[i][j] << " ";
-        }
         cout << "\n";
     }
 
