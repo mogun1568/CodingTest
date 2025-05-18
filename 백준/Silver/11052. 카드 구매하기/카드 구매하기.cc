a99b65ffde38c06e5b6398675a231ec83dce6fb7
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-05-18 16:07:34 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -1,26 +1,23 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
-int dp[1001];
+int DP[1001];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    int n;
-    cin >> n;
 
-    for (int i = 1; i <= n; i++) {
-        cin >> dp[i];
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> DP[i];
 
-        for (int j = 1; j <= i / 2; j++) {
-            dp[i] = max(dp[i], dp[j] + dp[i - j]);
-        }
+        for (int j = 1; j <= i / 2; j++)
+            DP[i] = max(DP[i], DP[j] + DP[i - j]);
     }
 
-    cout << dp[n];
+    cout << DP[N];
     
     return 0;
 }
