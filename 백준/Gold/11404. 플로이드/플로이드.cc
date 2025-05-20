e3e32e33c133ef41afb600c683b223f4ab30a952
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-05-20 21:51:34 +0900

    [Gold IV] Title: 플로이드, Time: 20 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,54 +1,46 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
-
-#define MAX 987654321
-
-int N, M, a, b, c;
+#define INF 10000000
 
 int city[101][101];
-
-void Floyd() {
-    for (int k = 1; k <= N; k++) {
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                city[i][j] = min(city[i][j], city[i][k] + city[k][j]);
-            }
-        }
-    }
-}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
+
+    int N, M, s, e, c;
     cin >> N >> M;
-    
+
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
-            if (i == j)
-                continue;
-            city[i][j] = MAX;
+            if (i != j)
+                city[i][j] = INF;
         }
     }
-    
+
     for (int i = 0; i < M; i++) {
-        cin >> a >> b >> c;
-        city[a][b] = min(city[a][b], c);
+        cin >> s >> e >> c;
+        city[s][e] = min(city[s][e], c);
     }
 
-    Floyd();
+    for (int k = 1; k <= N; k++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++)
+                city[i][j] = min(city[i][j], city[i][k] + city[k][j]);
+        }
+    }
 
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
-            if (city[i][j] == MAX)
-                cout << "0 ";
+            if (city[i][j] == INF)
+                cout << 0;
             else
-                cout << city[i][j] << " ";
+                cout << city[i][j];
+            cout << " ";
         }
         cout << "\n";
-    }    
+    }
     
     return 0;
 }
