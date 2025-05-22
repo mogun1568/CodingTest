4455b3a551e970953fc189f0397ae253bd7ef8b4
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-05-22 20:17:17 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -1,41 +1,38 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
-int N, answer;
 int arr[1000];
-int dp[1000][2];
+int DP[1000][2];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
+
+    int N, ans = 0;
     cin >> N;
     
     for (int i = 0; i < N; i++) {
         cin >> arr[i];
 
-        dp[i][0] = 1;
-        for (int j = i - 1; j >= 0; j--) {
-            if (arr[i] > arr[j]) {
-                dp[i][0] = max(dp[i][0], dp[j][0] + 1);
-            }
+        DP[i][0] = 1;
+        for (int j = 0; j < i; j++) {
+            if (arr[i] > arr[j])
+                DP[i][0] = max(DP[i][0], DP[j][0] + 1);
         }
     }
     
     for (int i = N - 1; i >= 0; i--) {
-        dp[i][1] = 1;
+        DP[i][1] = 1;
         for (int j = i + 1; j < N; j++) {
-            if (arr[i] > arr[j]) {
-                dp[i][1] = max(dp[i][1], dp[j][1] + 1);
-            }
+            if (arr[i] > arr[j])
+                DP[i][1] = max(DP[i][1], DP[j][1] + 1);
         }
 
-        answer = max(answer, dp[i][0] + dp[i][1] - 1);
+        ans = max(ans, DP[i][0] + DP[i][1] - 1);
     }
 
-    cout << answer;
+    cout << ans;
 
     return 0;
 }
