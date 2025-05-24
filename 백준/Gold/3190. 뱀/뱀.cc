605e93fecc896e92eef9aa2a049e966d02e47fa1
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-05-24 16:49:33 +0900

    [Gold IV] Title: 뱀, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.cc
+++ 백준/Gold/3190. 뱀/뱀.cc
@@ -1,75 +1,69 @@
 #include <iostream>
 #include <queue>
-
+#include <deque>
 using namespace std;
 
-int N, K, L;
+int N;
 int board[101][101];
 queue<pair<int, int>> dir;
 
-int dr[4] = {0, 1, 0, -1};
-int dc[4] = {1, 0, -1, 0};
-
-int Solve() {
-    queue<pair<int, int>> q;
-    board[1][1] = 1;
-    q.push({1, 1});
-    
-    int d = 0, sec = 0;
-    while (true) {
-        int nr = q.back().first + dr[d];
-        int nc = q.back().second + dc[d];
-
-        if (nr < 1 || nr > N || nc < 1 || nc > N)
-            break;
-
-        if (board[nr][nc] == 1)
-            break;
-
-        bool isApple = false;
-        if (board[nr][nc] == 2)
-            isApple = true;
-        
-        board[nr][nc] = 1;
-        q.push({nr, nc});
-                
-        if (!isApple) {
-            board[q.front().first][q.front().second] = 0;
-            q.pop();
-        }
-
-        sec++;
-        if (dir.front().first == sec) {
-            d = (d + dir.front().second) % 4;
-            dir.pop();
-        }
-    }
-    
-    return sec + 1;
-}
+int dr[4] = {-1, 0, 1, 0};
+int dc[4] = {0, 1, 0, -1};
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    int a, b, X;
-    char C;
-    
+
+    int K, r, c;
     cin >> N >> K;
-    for (int i = 0; i < K; i++) {
-        cin >> a >> b;
-        board[a][b] = 2;
+    while (K--) {
+        cin >> r >> c;
+        board[r][c] = 2;
     }
+
+    int L, X;
+    char C;
     cin >> L;
-    for (int i = 0; i < L; i++) {
+    while (L--) {
         cin >> X >> C;
+
         if (C == 'L')
             dir.push({X, 3});
         else
             dir.push({X, 1});
     }
 
-    cout << Solve();
+    deque<pair<int, int>> dq;
+    board[1][1] = 1;
+    dq.push_back({1, 1});
+    int sec = 0, d = 1;
+    
+    while (1) {
+        sec++;
+        
+        int nr = dq.front().first + dr[d];
+        int nc = dq.front().second + dc[d];
+
+        if (nr < 1 || nr > N || nc < 1 || nc > N)
+            break;
+
+        if (board[nr][nc] == 1)
+            break;
+        else if (board[nr][nc] == 0) {
+            board[dq.back().first][dq.back().second] = 0;
+            dq.pop_back();
+        }
+        
+        board[nr][nc] = 1;
+        dq.push_front({nr, nc});
+
+        if (!dir.empty() && sec == dir.front().first) {
+            d = (d + dir.front().second) % 4;
+            dir.pop();
+        }
+    }
+
+    cout << sec;
     
     return 0;
 }
