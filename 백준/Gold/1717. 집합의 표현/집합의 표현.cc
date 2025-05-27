fb846f5f5480854b540482d3322be015b81581ec
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-05-27 21:24:34 +0900

    [Gold V] Title: 집합의 표현, Time: 32 ms, Memory: 6324 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,56 +1,44 @@
 #include <iostream>
-
 using namespace std;
 
 int parent[1000001];
 
-int find(int x) {
-	if (parent[x] == x) {
-		return x;
-    }
-
-	parent[x] = find(parent[x]);
-
-	return parent[x];
+int Find(int x) {
+	if (parent[x] == x) return x;
+	return parent[x] = Find(parent[x]);
 }
 
-void merge(int x, int y) {
-	x = find(x);
-	y = find(y);
-
-	if (x == y) {
-        return;
-    }
-    
-	parent[x] = y;
+void Union(int x, int y) {
+	x = Find(x);
+	y = Find(y);
+	if (x < y) parent[y] = x;
+	else parent[x] = y;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, m, c, a, b;
-    cin >> n >> m;
-
-    for (int i = 0; i <= n; i++) {
+    int N, M;
+    cin >> N >> M;
+    
+    for (int i = 0; i <= N; i++)
         parent[i] = i;
-    }
 
+    int o, a, b;
+    while (M--) {
+        cin >> o >> a >> b;
 
-    for (int i = 0; i < m; i++) {
-        cin >> c >> a >> b;
-
-        if (c == 0) {
-            merge(a, b);
-        } else {
-            if (find(a) == find(b)) {
-				cout << "YES\n";
-            }
-			else {
-				cout << "NO\n";
-            }
+        if (!o)
+            Union(a, b);
+        else {
+            if (Find(a) == Find(b))
+                cout << "YES";
+            else
+                cout << "NO";
+            cout << "\n";
         }
     }
-
+    
     return 0;
 }
