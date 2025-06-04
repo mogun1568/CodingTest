c9482a791cbc6fcbe0c528c65ffcd72c3c6e0bf0
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-06-04 21:54:44 +0900

    [Gold V] Title: 탑, Time: 84 ms, Memory: 6108 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,31 +1,36 @@
 #include <iostream>
 #include <stack>
-
 using namespace std;
+
+int top[500001];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, h;
-    cin >> n;
+    int N;
+    cin >> N;
+    
+    stack<int> st;
+    for (int i = 1; i <= N; i++) {
+        cin >> top[i];
 
-    stack<pair<int, int>> s;
-    for (int i = 1; i <= n; i++) {
-        cin >> h;
-
-        while (!s.empty() && s.top().first <= h) {
-            s.pop();
+        while (1) {
+            if (st.empty()) {
+                cout << 0 << " ";
+                break;
+            }
+            
+            if (top[st.top()] < top[i])
+                st.pop();
+            else {
+                cout << st.top() << " ";
+                break;
+            }
         }
 
-        if (s.empty()) {
-            cout << "0 ";
-        } else {
-            cout << s.top().second << ' ';
-        }      
+        st.push(i);
+    } 
     
-        s.push({h, i});
-    }
-
     return 0;
 }
