25d5ce8841243f27f845a0d6b69011a436c57a9b
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-06-11 21:14:39 +0900

    [Gold IV] Title: 문자열 폭발, Time: 28 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,44 +1,43 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
 using namespace std;
-
-string S, B;
-vector<char> answer;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+
+    string s, bomb;
+    cin >> s >> bomb;
+
+    vector<char> ans;
+    int bLen = bomb.length();
+    bool check;
     
-    cin >> S >> B;
+    for (int i = 0; i < s.length(); i++) {
+        ans.push_back(s[i]);
 
-    int bLen = B.length();
-    for (int i = 0; i < S.length(); i++) {
-        answer.push_back(S[i]);
-
-        if (answer.size() >= bLen) {
-            bool check = true;
+        if (ans.size() >= bLen) {
+            check = true;
             for (int j = 0; j < bLen; j++) {
-                if (answer[answer.size() - bLen + j] != B[j]) {
+                if (ans[ans.size() - bLen + j] != bomb[j]) {
                     check = false;
                     break;
                 }
             }
+            
             if (check) {
-                for (int j = 0; j < bLen; j++) {
-                    answer.pop_back();
-                }
+                for (int j = 0; j < bLen; j++)
+                    ans.pop_back();
             }
         }
     }
 
-    if (answer.empty())
+    if (ans.empty())
         cout << "FRULA";
     else {
-        for (char c : answer) {
+        for (char c : ans)
             cout << c;
-        }
     }
 
     return 0;
