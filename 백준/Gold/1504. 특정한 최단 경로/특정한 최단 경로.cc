2bd3390b25ef1d2c102865024853553d58bc7540
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-06-12 19:59:24 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 44 ms, Memory: 5744 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -1,43 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
+#include <algorithm>
 using namespace std;
+#define MAX 64000000
 
-#define MAX 987654321
-
-int N, E, v1, v2, answer = MAX;
+int N;
 vector<pair<int, int>> graph[801];
 int visited[801];
 
-void Init() {
+void Clear() {
     for (int i = 1; i <= N; i++)
         visited[i] = MAX;
 }
 
-void Bfs(int start) {
-    Init();
+void BFS(int start) {
+    Clear();
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     visited[start] = 0;
     pq.push({0, start});
 
     while (!pq.empty()) {
-        int cp = pq.top().second;
-        int cd = pq.top().first;
+        int a = pq.top().second;
+        int c = pq.top().first;
         pq.pop();
 
-        if (cd > visited[cp])
+        if (c > visited[a])
             continue;
 
-        for (int i = 0; i < graph[cp].size(); i++) {
-            int np = graph[cp][i].first;
-            int nd = cd + graph[cp][i].second;
+        for (int i = 0; i < graph[a].size(); i++) {
+            int b = graph[a][i].first;
+            int nc = c + graph[a][i].second;
 
-            if (nd > visited[np])
+            if (nc > visited[b])
                 continue;
 
-            visited[np] = nd;
-            pq.push({nd, np});
+            visited[b] = nc;
+            pq.push({nc, b});
         }
     }
 }
@@ -45,39 +44,35 @@ void Bfs(int start) {
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    int a, b, c;
+
+    int E, a, b, c, v1, v2;
     cin >> N >> E;
-    for (int i = 0; i < E; i++) {
+    while (E--) {
         cin >> a >> b >> c;
         graph[a].push_back({b, c});
         graph[b].push_back({a, c});
     }
     cin >> v1 >> v2;
 
-    if (E == 0) {
-        cout << -1;
-        return 0;
-    }
-
-    Bfs(1);
+    BFS(1);
     int oneTov1 = visited[v1];
     int oneTov2 = visited[v2];
 
-    Bfs(v1);
+    BFS(v1);
     int v1Tov2 = visited[v2];
     int v1ToN = visited[N];
 
-    Bfs(v2);
+    BFS(v2);
     int v2ToN = visited[N];
- 
-    answer = min(answer, oneTov1 + v1Tov2 + v2ToN);
-    answer = min(answer, oneTov2 + v1Tov2 + v1ToN);
 
-    if (answer == MAX)
+    int ans = MAX;
+    ans = min(ans, oneTov1 + v1Tov2 + v2ToN);
+    ans = min(ans, oneTov2 + v1Tov2 + v1ToN);
+
+    if (ans == MAX)
         cout << -1;
     else
-        cout << answer;
+        cout << ans;
     
     return 0;
 }
