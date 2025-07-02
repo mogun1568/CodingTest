3cc40346e8dfa8722836b4bc44eeed78dc2c0359
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-07-02 18:01:01 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.cc
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -1,31 +1,24 @@
 #include <iostream>
-
 using namespace std;
 
-int dp[201][201];
+int DP[201][201];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, k;
-    cin >> n >> k;
+    int N, K;
+    cin >> N >> K;
 
-    for (int i = 0; i <= n; i++) {
-        dp[1][i] = 1;
-    }
-    for (int i = 1; i <= k; i++) {
-        dp[i][0] = 1;
-    }
+    for (int i = 1; i <= K; i++)
+        DP[1][i] = i;
 
-    for (int i = 2; i <= k; i++) {
-        for (int j = 1; j <= n; j++) {
-            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            dp[i][j] %= 1000000000;
-        }
+    for (int i = 2; i <= N; i++) {
+        for (int j = 1; j <= K; j++)
+            DP[i][j] = (DP[i - 1][j] + DP[i][j - 1]) % 1000000000;
     }
 
-    cout << dp[k][n];    
+    cout << DP[N][K];    
 
     return 0;
 }
