0fc25cb63be673acf7f8f644874ca004d8c2772d
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-07-06 13:34:57 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.cc
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-int dp[501];
+int DP[100];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, a, b, result = 0;
+    int n, a, b, ans = 0;
     cin >> n;
 
     vector<pair<int, int>> v;
@@ -21,16 +20,15 @@ int main() {
     sort(v.begin(), v.end());
 
     for (int i = 0; i < n; i++) {
-        dp[i] = 1;
+        DP[i] = 1;
         for (int j = 0; j < i; j++) {
-            if (v[j].second < v[i].second) {
-                dp[i] = max(dp[i], dp[j] + 1);
-            }
+            if (v[j].second < v[i].second)
+                DP[i] = max(DP[i], DP[j] + 1);
         }
-        result = max(result, dp[i]);
+        ans = max(ans, DP[i]);
     }
 
-    cout << n - result;      
+    cout << n - ans;      
 
     return 0;
 }
