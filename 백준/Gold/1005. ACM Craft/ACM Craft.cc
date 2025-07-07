d851e80dcb90fdf143bc292001519fa039ce22b2
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-07-07 21:24:05 +0900

    [Gold III] Title: ACM Craft, Time: 144 ms, Memory: 2852 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.cc
+++ 백준/Gold/1005. ACM Craft/ACM Craft.cc
@@ -1,20 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
+#include <algorithm>
 using namespace std;
 
-int T, N, K, W;
+int N;
 int D[1001];
+int DP[1001];
 vector<int> v[1001];
-int preOrder[1001];
-int dp[1001];
+int inDegree[1001];
 
 void Init() {
     for (int i = 1; i <= N; i++) {
         v[i].clear();
-        preOrder[i] = 0;
-        dp[i] = 0;
+        inDegree[i] = 0;
+    }
+}
+
+void BFS() {
+    queue<int> q;
+    for (int i = 1; i <= N; i++) {
+        if (inDegree[i] == 0)
+            q.push(i);
+    }
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        
+        for (int i = 0; i < v[cur].size(); i++) {
+            int next = v[cur][i];
+            DP[next] = max(DP[next], DP[cur] + D[next]);
+            
+            inDegree[next]--;
+            if (inDegree[next] == 0)
+                q.push(next);
+        }
     }
 }
 
@@ -22,47 +43,28 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
+    int T, K, X, Y, W;
     cin >> T;
 
     while (T--) {
         cin >> N >> K;
+
         for (int i = 1; i <= N; i++) {
             cin >> D[i];
-            dp[i] = D[i];
+            DP[i] = D[i];
         }
         
-        int x, y;
         while (K--) {
-            cin >> x >> y;
-
-            v[x].push_back(y);
-            preOrder[y]++;
+            cin >> X >> Y;
+            v[X].push_back(Y);
+            inDegree[Y]++;
         }
+
         cin >> W;
+    
+        BFS();
 
-        queue<int> q;
-        for (int i = 1; i <= N; i++) {
-            if (preOrder[i] == 0)
-                q.push(i);
-        }
-
-        while (!q.empty()) {
-            int cur = q.front();
-            q.pop();
- 
-            for (int i = 0; i < v[cur].size(); i++) {
-                int next = v[cur][i];
-                
-                dp[next] = max(dp[next], dp[cur] + D[next]);
-                preOrder[next]--;
- 
-                if (preOrder[next] == 0) {
-                    q.push(next);
-                }
-            }
-        }
-
-        cout << dp[W] << "\n";
+        cout << DP[W] << "\n";
 
         Init();
     }
