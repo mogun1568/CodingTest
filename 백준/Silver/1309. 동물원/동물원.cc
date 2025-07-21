aacc987ed3ff8338f0fd403de90a92d4ac2c9023
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-07-21 19:40:50 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.cc
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -1,24 +1,21 @@
 #include <iostream>
-
 using namespace std;
-
 #define MOD 9901
 
-int dp[100001] = {1, 3};
+int DP[100001] = {1, 3};
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int n;
-    cin >> n;
-
-    for (int i = 2; i <= n; i++) {
-        dp[i] = 2 * dp[i - 1] + dp[i - 2];
-        dp[i] %= MOD;
+    int N;
+    cin >> N;
+    for (int i = 2; i <= N; i++) {
+        DP[i] = 2 * DP[i - 1] + DP[i - 2];
+        DP[i] %= MOD;
     }
 
-    cout << dp[n];
+    cout << DP[N];
 
     return 0;
 }
