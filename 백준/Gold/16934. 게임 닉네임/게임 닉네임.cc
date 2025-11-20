f5634727f000e5c4f1de7a9e45c6001e11fccbd9
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-11-20 21:11:10 +0900

    [Gold III] Title: 게임 닉네임, Time: 152 ms, Memory: 72024 KB -BaekjoonHub

---
+++ 백준/Gold/16934. 게임 닉네임/게임 닉네임.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<string, int> nickname;
+
+struct Trie {
+    Trie* children[26];
+
+    Trie() {
+        for (int i = 0; i < 26; i++)
+            children[i] = nullptr;
+    }
+
+    ~Trie() {
+        for (int i = 0; i < 26; i++)
+            delete children[i];
+    }
+
+    void insert(const string& str) {
+        Trie* cur = this;
+        string result = "";
+        bool isFirst = false;
+        
+        for (char c : str) {
+            if (!isFirst)
+                result += c;
+            
+            int idx = c - 'a';
+            if (cur -> children[idx] == nullptr) {
+                cur -> children[idx] = new Trie();
+                isFirst = true;
+            }
+            cur = cur -> children[idx];
+        }
+
+        cout << result;
+        if (!isFirst && nickname.find(str) != nickname.end())
+            cout << nickname[str] + 1;
+        cout << "\n";
+        
+        nickname[str]++;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    Trie root;
+    string str;
+    while (N--) {
+        cin >> str;
+        root.insert(str);
+    }
+    
+    return 0;
+}
