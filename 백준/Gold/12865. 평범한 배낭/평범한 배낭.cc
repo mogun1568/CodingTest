a3207f52d851aefbe61b7c08486c566efa2b610b
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-11-21 19:22:13 +0900

    [Gold V] Title: 평범한 배낭, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,30 +1,24 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
-int DP[101][100001];
-int W[101], V[101];
+int DP[100001];
 
 int main() {
-    ios_base::sync_with_stdio(0);
+    ios::sync_with_stdio(0);
     cin.tie(0);
 
     int N, K;
     cin >> N >> K;
-    for (int i = 1; i <= N; i++)
-        cin >> W[i] >> V[i];
 
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= K; j++) {
-            if (j >= W[i])
-                DP[i][j] = max(DP[i - 1][j], DP[i - 1][j - W[i]] + V[i]);
-            else
-                DP[i][j] = DP[i - 1][j];
-        }
+    int W, V;
+    while(N--) {
+        cin >> W >> V;
+        for (int j = K; j >= W; j--)
+            DP[j] = max(DP[j], DP[j - W] + V);
     }
 
-    cout << DP[N][K];
+    cout << DP[K];
     
     return 0;
 }
