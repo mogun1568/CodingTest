8ac706cb5f2039e5eff9965d54e3a3ddd04dc26a
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-11-21 22:38:26 +0900

    [Gold III] Title: 앱, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -1,41 +1,38 @@
-#include<iostream>
-#include<algorithm>
-
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int N, M, sum;
-int bite[101], cost[101];
-int dp[101][10001];
+int memory[101];
+int cost[101];
+int DP[10001];
 
-int main()
-{
-    ios_base::sync_with_stdio(0);
+int main() {
+    ios::sync_with_stdio(0);
     cin.tie(0);
+
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+        cin >> memory[i];
+
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> cost[i];
+        sum += cost[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = sum; j >= cost[i]; j--)
+            DP[j] = max(DP[j], DP[j - cost[i]] + memory[i]);
+    }
+
+    for (int i = 0; i <= sum; i++) {
+        if (DP[i] >= M) {
+            cout << i;
+            break;
+        }
+    }
     
-	cin >> N >> M;
-	for (int i = 1; i <= N; i++) 
-        cin >> bite[i];
-	for (int i = 1; i <= N; i++)  {
-		cin >> cost[i];
-		// sum : 모든 비용들의 합
-		sum += cost[i];
-	}
-
-	// dp[i][j] == i번째 앱까지 탐색했을 때 
-	// j 비용을 소모해서 얻을 수 있는 최대 메모리
-	for (int i = 1; i <= N; i++) {
-		for (int j = 0; j <= sum; j++) {
-			if (j - cost[i] >= 0)
-				dp[i][j] = max(dp[i][j], dp[i - 1][j - cost[i]] + bite[i]);
-			
-			dp[i][j] = max(dp[i][j], dp[i - 1][j]);
-		}
-	}
-
-	for (int i = 0; i <= sum; i++) {
-		if (dp[N][i] >= M) {
-			cout << i;
-			break;
-		}	
-	}
+    return 0;
 }
