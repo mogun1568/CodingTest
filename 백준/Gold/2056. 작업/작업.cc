b878480c9f63775ebc0fe2d1f3117fcbcb0b91a1
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2025-12-10 16:14:28 +0900

    [Gold IV] Title: 작업, Time: 40 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.cc
+++ 백준/Gold/2056. 작업/작업.cc
@@ -4,12 +4,7 @@
 #include <algorithm>
 using namespace std;
 
-const int MAX = 10001;
-
-vector<int> v[MAX];
-int inDgree[MAX];
-int workTime[MAX];
-int DP[MAX];
+int DP[10001];
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -18,38 +13,18 @@ int main() {
     int N;
     cin >> N;
 
-    int a, b, c;
+    int workTime, workCnt, preNum, ans = 0;
     for (int i = 1; i <= N; i++) {
-        cin >> a >> b;
-        workTime[i] = DP[i] = a;
+        cin >> workTime >> workCnt;
 
-        while (b--) {
-            cin >> c;
-            v[c].push_back(i);
-            inDgree[i]++;
+        int maxTime = 0;
+        while (workCnt--) {
+            cin >> preNum;
+            maxTime = max(maxTime, DP[preNum]);
         }
-    }
 
-    queue<int> q;
-    for (int i = 1; i <= N; i++) {
-        if (inDgree[i] == 0)
-            q.push(i);
-    }
-
-    int ans = 0;
-    while (!q.empty()) {
-        int cur = q.front();
-        q.pop();
-
-        ans = max(ans, DP[cur]);
-
-        for (int i = 0; i < v[cur].size(); i++) {
-            int next = v[cur][i];
-            DP[next] = max(DP[next], DP[cur] + workTime[next]);
-
-            if (--inDgree[next] == 0)
-                q.push(next);
-        }
+        DP[i] = maxTime + workTime;
+        ans = max(ans, DP[i]);
     }
 
     cout << ans;
