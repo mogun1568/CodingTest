0d5e0cbe67636ee12720078acaaf51f4360526a4
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2026-03-31 19:11:41 +0900

    [Gold V] Title: 회의실 배정, Time: 20 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -1,39 +1,30 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
-pair<int, int> meet[100000];
-
-bool cmp(const pair<int, int>& a, const pair<int, int>& b) {
-    if (a.second == b.second)
-        return a.first < b.first;
-    return a.second < b.second;
-}
+pair<int, int> room[100001];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int N, s, e;
+    int N;
     cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> s >> e;
-        meet[i].first = s;
-        meet[i].second = e;
-    }
 
-    sort(meet, meet + N, cmp);
+    for (int i = 0; i < N; i++)
+        cin >> room[i].second >> room[i].first;
+    sort(room, room + N);
 
-    int ans = 1, curE = meet[0].second;
+    int cnt = 1, end = room[0].first;
     for (int i = 1; i < N; i++) {
-        if (meet[i].first >= curE) {
-            ans++;
-            curE = meet[i].second;
-        }
+        if (room[i].second < end)
+            continue;
+
+        cnt++;
+        end = room[i].first;
     }
 
-    cout << ans;
+    cout << cnt;
     
     return 0;
 }
