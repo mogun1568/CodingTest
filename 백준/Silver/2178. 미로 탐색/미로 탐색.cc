e921570d14bf11be53c367ff12dd300d488c77e8
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2026-03-31 15:53:45 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2196 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,57 +1,59 @@
 #include <iostream>
+#include <string>
 #include <queue>
-
 using namespace std;
 
-int n, m;
+int N, M;
+int maze[101][101];
 
-int map[101][101];
+int dr[] = {-1, 1, 0, 0};
+int dc[] = {0, 0, -1, 1};
 
-int dr[4] = {-1, 1, 0, 0};
-int dc[4] = {0, 0, -1, 1};
-
-int bfs(int r, int c) {
+int BFS() {
     queue<pair<int, int>> q;
-    q.push({r, c});
+    maze[1][1] = 2;
+    q.push({1, 1});
 
     while (!q.empty()) {
         int r = q.front().first;
         int c = q.front().second;
         q.pop();
 
+        if (r == N && c == M)
+            return maze[r][c] - 1;
+
         for (int i = 0; i < 4; i++) {
-            int curR = r + dr[i];
-            int curC = c + dc[i];
+            int nr = r + dr[i];
+            int nc = c + dc[i];
 
-            if (curR < 1 || curR > n || curC < 1 || curC > m) {
+            if (nr < 1 || nr > N || nc < 1 || nc > M)
                 continue;
-            }
 
-            if (map[curR][curC] == 1) {
-                map[curR][curC] = map[r][c] + 1;
-                q.push({curR, curC});
-            }
-        } 
+            if (maze[nr][nc] != 1)
+                continue;
+
+            maze[nr][nc] = maze[r][c] + 1;
+            q.push({nr, nc});
+        }
     }
 
-    return map[n][m];
+    return 0;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    cin >> n >> m;
 
-    string s;
-    for (int i = 1; i <= n; i++) {
-        cin >> s;
-        for (int j = 1; j <= m; j++) {
-            map[i][j] = s[j - 1] - '0';
-        }
+    cin >> N >> M;
+
+    string str;
+    for (int i = 1; i <= N; i++) {
+        cin >> str;
+        for (int j = 1; j <= M; j++)
+            maze[i][j] = str[j - 1] - '0';
     }
 
-    cout << bfs(1, 1);
+    cout << BFS();
     
     return 0;
 }
