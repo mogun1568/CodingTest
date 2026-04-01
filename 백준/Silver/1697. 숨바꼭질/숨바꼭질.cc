1d3e6d96b2541f13063a89cccf20736e96267b27
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2026-04-01 14:12:17 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2384 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,35 +1,36 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-#define MAX 100000
+#define MAX 100001
 
 bool visited[MAX];
+queue<pair<int, int>> q;
+
+void TryVisit(int next, int sec) {
+    if (next < 0 || next >= MAX)
+        return;
+    
+    if (!visited[next]) {
+        visited[next] = true;
+        q.push({next, sec + 1});
+    }
+}
 
 int BFS(int n, int k) {
-    queue<pair<int, int>> q;
     visited[n] = true;
     q.push({n, 0});
 
     while (!q.empty()) {
-        int c = q.front().first;
-        int t = q.front().second;
+        int cur = q.front().first;
+        int sec = q.front().second;
         q.pop();
 
-        if (c == k)
-            return t;
+        if (cur == k)
+            return sec;
 
-        if (c - 1 >= 0 && !visited[c - 1]) {
-            visited[c - 1] = true;
-            q.push({c - 1, t + 1});
-        }
-        if (c + 1 <= MAX && !visited[c + 1]) {
-            visited[c + 1] = true;
-            q.push({c + 1, t + 1});
-        }
-        if (c * 2 <= MAX && !visited[c * 2]) {
-            visited[c * 2] = true;
-            q.push({c * 2, t + 1});
-        }
+        TryVisit(cur - 1, sec);
+        TryVisit(cur + 1, sec);
+        TryVisit(2 * cur, sec);
     }
 
     return 0;
@@ -38,10 +39,10 @@ int BFS(int n, int k) {
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int N, K;
     cin >> N >> K;
-    
+
     cout << BFS(N, K);
     
     return 0;
