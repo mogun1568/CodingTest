e355d950e83006ae142ff4bd50ba7086783fb7bc
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2026-04-01 13:53:27 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
-int RGB[1001][3];
+int house[1001][3];
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -11,15 +10,16 @@ int main() {
 
     int N;
     cin >> N;
-    for (int i = 1; i <= N; i++) {
-        cin >> RGB[i][0] >> RGB[i][1] >> RGB[i][2];
 
-        RGB[i][0] += min(RGB[i - 1][1], RGB[i - 1][2]);
-        RGB[i][1] += min(RGB[i - 1][0], RGB[i - 1][2]);
-        RGB[i][2] += min(RGB[i - 1][0], RGB[i - 1][1]);
+    for (int i = 1; i <= N; i++) {
+        cin >> house[i][0] >> house[i][1] >> house[i][2];
+
+        house[i][0] += min(house[i - 1][1], house[i - 1][2]);
+        house[i][1] += min(house[i - 1][0], house[i - 1][2]);
+        house[i][2] += min(house[i - 1][0], house[i - 1][1]);
     }
 
-    cout << min(RGB[N][0], min(RGB[N][1], RGB[N][2]));
+    cout << min(house[N][0], min(house[N][1], house[N][2]));
     
     return 0;
 }
