a2c6f402e335d983224ecd372ddc4900a7684841
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2026-04-02 17:20:32 +0900

    [Gold IV] Title: N-Queen, Time: 5264 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 int N, ans;
-int col[15];
+int col[16];
 
 void DFS(int r) {
     if (r == N) {
@@ -14,11 +14,9 @@ void DFS(int r) {
     for (int i = 0; i < N; i++) {
         col[r] = i;
         bool check = false;
-        
+
         for (int j = 0; j < r; j++) {
-            if(col[r] == col[j] || abs(col[r] - col[j]) == r - j){
-                // 1. 같은 행에 있는가
-                // 2. 대각선에 있는가
+            if (col[r] == col[j] || abs(col[r] - col[j]) == r - j) {
                 check = true;
                 break;
             }
@@ -32,9 +30,8 @@ void DFS(int r) {
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    cin >> N;
 
+    cin >> N;
     DFS(0);
 
     cout << ans;
