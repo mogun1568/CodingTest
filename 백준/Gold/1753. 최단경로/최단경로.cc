f58a01ee3b1f3b355eca61f09cc6d26f0f71ae66
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2026-04-03 16:49:52 +0900

    [Gold IV] Title: 최단경로, Time: 92 ms, Memory: 9184 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,34 +1,34 @@
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
 using namespace std;
-#define INF 3000000
 
-vector<pair<int, int>> edge[20001];
-int weight[20001];
+const int INF = 2e9;
 
-void Dijkstra(int k) {
+vector<pair<int, int>> graph[20001];
+int visited[20001];
+
+void dijkstra(int k) {
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    weight[k] = 0;
+    visited[k] = 0;
     pq.push({0, k});
 
     while (!pq.empty()) {
-        int u = pq.top().second;
+        int cur = pq.top().second;
         int w = pq.top().first;
         pq.pop();
-        
-        if (weight[u] < w)
+
+        if (w > visited[cur])
             continue;
 
-        for (int i = 0; i < edge[u].size(); i++) {
-            int v = edge[u][i].first;
-            int nw = w + edge[u][i].second;
+        for (int i = 0; i < graph[cur].size(); i++) {
+            int next = graph[cur][i].first;
+            int nw = w + graph[cur][i].second;
 
-            if (weight[v] <= nw)
-                continue;
-
-            weight[v] = nw;
-            pq.push({nw, v});
+            if (nw < visited[next]) {
+                visited[next] = nw;
+                pq.push({nw, next});
+            }
         }
     }
 }
@@ -37,23 +37,23 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int V, E, K, u, v, w;
+    int V, E, K;
     cin >> V >> E >> K;
-    for (int i = 0; i < E; i++) {
+
+    int u, v, w;
+    while (E--) {
         cin >> u >> v >> w;
-        edge[u].push_back({v, w});
+        graph[u].push_back({v, w});
     }
 
     for (int i = 1; i <= V; i++)
-        weight[i] = INF;
+        visited[i] = INF;
 
-    Dijkstra(K);
+    dijkstra(K);
 
     for (int i = 1; i <= V; i++) {
-        if (weight[i] == INF)
-            cout << "INF";
-        else
-            cout << weight[i];
+        if (visited[i] == INF) cout << "INF";
+        else cout << visited[i];
         cout << "\n";
     }
     
