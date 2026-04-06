7674fa19d945fadb13793db1b33551fa712b599e
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2026-04-06 15:45:52 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.cc
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -1,46 +1,46 @@
 #include <iostream>
 using namespace std;
 
-char tree[26][2];
+int N;
+pair<char, char> tree[26];
 
 void Pre(char cur) {
     if (cur == '.')
         return;
-    
+
     cout << cur;
-    Pre(tree[cur - 'A'][0]);
-    Pre(tree[cur - 'A'][1]);
+    Pre(tree[cur - 'A'].first);
+    Pre(tree[cur - 'A'].second);
 }
 
 void In(char cur) {
     if (cur == '.')
         return;
-    
-    In(tree[cur - 'A'][0]);
+
+    In(tree[cur - 'A'].first);
     cout << cur;
-    In(tree[cur - 'A'][1]);
+    In(tree[cur - 'A'].second);
 }
 
 void Post(char cur) {
     if (cur == '.')
         return;
-    
-    Post(tree[cur - 'A'][0]);
-    Post(tree[cur - 'A'][1]);
+
+    Post(tree[cur - 'A'].first);
+    Post(tree[cur - 'A'].second);
     cout << cur;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    int N;
+
     cin >> N;
 
-    char root;
+    char node;
     for (int i = 0; i < N; i++)
-        cin >> root >> tree[root - 'A'][0] >> tree[root - 'A'][1];
-        
+        cin >> node >> tree[node - 'A'].first >> tree[node - 'A'].second;
+
     Pre('A');
     cout << "\n";
     In('A');
