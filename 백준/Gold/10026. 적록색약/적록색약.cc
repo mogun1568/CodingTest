91b95fdf3fc48ee595d18faab116f690a3fff644
mogun1568 <96761776+mogun1568@users.noreply.github.com>
2026-04-06 15:27:09 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -5,19 +5,19 @@
 using namespace std;
 
 int N;
-char area[100][100];
-bool visited[100][100];
+string RGB[101];
+bool visited[101][101];
 
-int dr[4] = {-1, 1, 0, 0};
-int dc[4] = {0, 0, -1, 1};
+int dr[] = {-1, 1, 0, 0};
+int dc[] = {0, 0, -1, 1};
 
 void BFS(int r, int c) {
-    char cur = area[r][c];
+    char color = RGB[r][c];
     queue<pair<int, int>> q;
     visited[r][c] = true;
     q.push({r, c});
 
-    while(!q.empty()) {
+    while (!q.empty()) {
         int cr = q.front().first;
         int cc = q.front().second;
         q.pop();
@@ -29,7 +29,10 @@ void BFS(int r, int c) {
             if (nr < 0 || nr >= N || nc < 0 || nc >= N)
                 continue;
 
-            if (visited[nr][nc] || area[nr][nc] != cur)
+            if (RGB[nr][nc] != color)
+                continue;
+
+            if (visited[nr][nc])
                 continue;
 
             visited[nr][nc] = true;
@@ -43,46 +46,40 @@ int main() {
     cin.tie(0);
 
     cin >> N;
-
-    string s;
-    for (int i = 0; i < N; i++) {
-        cin >> s;
-        for (int j = 0; j < N; j++)
-            area[i][j] = s[j];
-    }
+    for (int i = 0; i < N; i++)
+        cin >> RGB[i];
 
     int cnt = 0;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            if (visited[i][j])
-                continue;
-
-            cnt++;
-            BFS(i, j);
+            if (!visited[i][j]) {
+                cnt++;
+                BFS(i, j);
+            }
         }
     }
-    cout << cnt << "\n";
-    
+
+    cout << cnt << " ";
+
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            if (area[i][j] == 'R')
-                area[i][j] = 'G';
+            if (RGB[i][j] == 'G')
+                RGB[i][j] = 'R';
         }
     }
-
-    cnt = 0;
     memset(visited, false, sizeof(visited));
     
+    cnt = 0;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            if (visited[i][j])
-                continue;
-
-            cnt++;
-            BFS(i, j);
+            if (!visited[i][j]) {
+                cnt++;
+                BFS(i, j);
+            }
         }
     }
-    cout << cnt << "\n";
+
+    cout << cnt;
     
     return 0;
 }
